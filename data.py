c3131a3eb0c054edba1bfd11ddf9c7430f44d0af
devyani-wagh <devyani.jitndra@avantika.edu.in>
2026-03-19 16:25:50 +0530

    Added Task 4 evidence

---
+++ data.py
@@ -0,0 +1,20 @@
+x = 10
+print("Before:", id(x))
+
+x = 15
+print("After:", id(x))
+
+name = "Devyani"
+print("Before:", id(name))
+
+name = "Wagh"
+print("After:", id(name))
+
+
+nums = [28, 30]
+print("Before:", id(nums))
+
+nums.append(30)
+print("After:", id(nums))
+
+
